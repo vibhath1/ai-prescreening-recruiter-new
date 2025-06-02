ecd163f1dabccaf65371e1547febda73a2e08108
vibhath1 <149695142+vibhath1@users.noreply.github.com>
2025-06-02 17:30:16 +0530

    Update user.py

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel, EmailStr
-
 class UserCreate(BaseModel):
     username: str
-    email:    EmailStr
+    email: EmailStr
     password: str
+    role: str = "candidate"  # Add this with default
 
 class UserOut(BaseModel):
     id: int
     username: str
     email: EmailStr
+    role: str
 
     class Config:
-        orm_mode = True      # allows returning SQLAlchemy objects directly
+        orm_mode = True
